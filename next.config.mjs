eb9ff28f6efa9d9cdf391ede7572d099b440b87e
alvian21 <alvian.ardhiansyah@gmail.com>
2024-11-14 09:29:48 +0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (
-      config,
-      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-      if (config.cache && !dev) {
-        config.cache = Object.freeze({
-          type: 'memory',
-        })
-        config.cache.maxMemoryGenerations = 0
-      }
-      // Important: return the modified config
-      return config
+    experimental: {
+        cacheMaxMemorySize: 52428800, // 50 MB dalam byte
     },
-  }
-   
-  export default nextConfig
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory',
+            })
+            // config.cache.maxMemoryGenerations = 0
+        }
+        // Important: return the modified config
+        return config
+    },
+}
+
+export default nextConfig
